b3439646f8db3bbbcbe306cfcaff59e49bfb76ee
Tallot <o.maksymenko@arvilab.com>
2018-09-18 11:09:39 +0300

    Generators output in byte form

--- generators.py
+++ generators.py
@@ -9,7 +9,7 @@ def standard(n):
 	'''
 	seq = [random.getrandbits(8) for _ in range(n)]
 	return seq
-	
+
 def lehmer_low(n, x0=None):
 	if x0 is None:
 		x0 = int(time.time()) # random initialization
@@ -23,9 +23,9 @@ def lehmer_low(n, x0=None):
 		x_prev = x_next
 		x_next = '{:032b}'.format(x_next)[-8:] # get 8 lowest bits
 		seq.append(int(x_next,2)) # append value in decimal form
-		
+
 	return seq
-	
+
 def lehmer_high(n, x0=None):
 	if x0 is None:
 		x0 = int(time.time()) # random initialization
@@ -39,51 +39,62 @@ def lehmer_high(n, x0=None):
 		x_prev = x_next
 		x_next = '{:032b}'.format(x_next)[:8] # get 8 lowest bits
 		seq.append(int(x_next,2)) # append value in decimal form
-		
+
 	return seq
-	
+
 def shift_register(array, x):
 	array = array[1:]
 	array.append(x)
 	return array
-	
+
 def L20(n):
 	init = [random.getrandbits(1) for _ in range(19)]
 	seq = []
-	
-	for i in range(n):
-		x_t = init[17]^init[15]^init[11]^init[0]
+
+	buffer = []
+
+	for i in range(1, n+1):
+		x_t = init[17]^init[15]^init[11]^init[0] #produce bits
 		init = shift_register(init, x_t)
-		seq.append(x_t)
-		
+		buffer.append(x_t)
+		if i%8 == 0:
+			x_byte = int(''.join([srt(x) for x in buffer]), 2) # byte in decimal form
+			seq.append(x_byte)
+			del buffer[:] # clear buffer
+
 	return seq
-		
+
 def L89(n):
 	init = [random.getrandbits(1) for _ in range(89)]
 	seq = []
-	
-	for i in range(n):
+
+	buffer = []
+
+	for i in range(1, n+1):
 		x_t = init[51]^init[0]
 		init = shift_register(init, x_t)
-		seq.append(x_t)
-		
+		buffer.append(x_t)
+		if i%8 == 0:
+			x_byte = int(''.join([srt(x) for x in buffer]), 2) # byte in decimal form
+			seq.append(x_byte)
+			del buffer[:]
+
 	return seq
 
 def wolfram(n):
-	
+
 	r0 = int(time.time())
 	seq = []
-	x0 = '{:032b}'.format(r0)[-1]
-	seq.append(x0)
-	
-	for i in range(n):
+
+	buffer = []
+
+	for i in range(1, n+1):
 		r0 = (r0<<1)^(r0|(r0>>1))
 		x_i = '{:032b}'.format(r0)[-1]
-		seq.append(x_i)
-	
+		buffer.append(x_i)
+		if i%8 == 0:
+			x_byte = int(''.join([srt(x) for x in buffer]), 2) # byte in decimal form
+			seq.append(x_byte)
+			del buffer[:]
+
 	return seq
-	
-	
-	
-	
-	
